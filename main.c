90a848c91b33fc4a7d7daf4bdf0d8b2bf8125bf3
Ivar Trygve Jarlsby <ivar.jarlsby@gmail.com>
2014-05-19 00:06:05 +0200

    Removed non-essential files and header includes.

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
 #include "args.h"
 #include "log.h"
 
--- unix-socket.h
+++
@@ -1,8 +0,0 @@
-#ifndef UNIX_SOCKET_H
-#define UNIX_SOCKET_H
-
-int unix_is_socket(const char *path);
-const char *unix_strerror(void);
-int unix_listen(const char *path);
-
-#endif
